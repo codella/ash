cbd21eedb1b4b60d8206389cbc3ec13a2b676747
Mauro Codella <mauro.codella@gmail.com>
2020-03-02 22:50:07 +0100

    Augment test coverage for 'head'

--- src/array/head.test.ts
+++ src/array/head.test.ts
@@ -1,8 +1,18 @@
 import { head } from "./head";
 
 describe("head", () => {
-  it("returns the head of a non-empty array", () => {
+  it("returns undefined as the head of an empty array", () => {
+    const result = head([]);
+    expect(result).toBe(undefined);
+  });
+
+  it("returns the head of an array with a single element", () => {
     const result = head([1]);
+    expect(result).toBe(1);
+  });
+
+  it("returns the head of an array with multiple elements", () => {
+    const result = head([1, 2]);
     expect(result).toBe(1);
   });
 });
